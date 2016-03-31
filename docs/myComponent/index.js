d7978b0fbefad383bd12c446e3e28103a2f871c0
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2016-03-31 18:55:03 +0200

    added description for usage

--- docs/myComponent/index.js
+++ docs/myComponent/index.js
@@ -4,4 +4,8 @@ import MyComponentB from './myComponentB';
 import MyComponentDefault from './myComponentDefault';
 import { ToggleComponent } from '../../src';
 
+// This is the place where you activate the toggle for your component.
+// Provide a default component as a first component, and a list of arbitrary
+// variations. React Feature Toggle will activate the variation using the display
+// name of each variation, so be consistent with it.
 export default ToggleComponent(MyComponentDefault, MyComponentA, MyComponentB);
